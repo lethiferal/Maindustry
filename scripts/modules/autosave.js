9f02ddbcbad36974a222189bf9b710dbfbe73687
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-10-14 23:34:43 +0530

    Fixed auto-save not changing game state.

--- scripts/modules/autosave.js
+++ scripts/modules/autosave.js
@@ -1,9 +1,9 @@
 const file = Vars.saveDirectory.child('autosave' + '.' + Vars.saveExtension);
-
 Events.on(ServerLoadEvent, (e) => {
   if (Core.settings.get('loadAutoSave', '') === 'true'){
     Core.app.post(() => {
       SaveIO.load(file);
+      Vars.state.set(GameState.State.playing);
       Log.info('Auto-save recovered.');
     });
 }});
@@ -13,7 +13,7 @@ Timer.schedule(
     if (Core.settings.get('loadAutoSave', '') === 'true'){
       Core.app.post(() => {
         SaveIO.save(file);
-        Call.sendMessage('[lime] [white]| Game saved.');
+        Call.infoToast("[#7FD7FD7f]",4);
     })};
   },
   60,
