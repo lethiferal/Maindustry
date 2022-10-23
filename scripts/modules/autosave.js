0b84c29a5dd0af4be0e1a31a8579bf3fde7485b1
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-10-23 20:48:13 +0530

    Pauses the server when the map is loaded
    + pauses when player count is 0.
    + resumes when a player joins.

--- scripts/modules/autosave.js
+++ scripts/modules/autosave.js
@@ -1,12 +1,44 @@
-const file = Vars.saveDirectory.child('autosave' + '.' + Vars.saveExtension);
+let file;
+let playerCount;
+let pausedState;
+
+file = Vars.saveDirectory.child('autosave' + '.' + Vars.saveExtension);
+
 Events.on(ServerLoadEvent, (e) => {
   if (Core.settings.get('loadAutoSave', '') === 'true'){
+    playerCount = Groups.player.size();
     Core.app.post(() => {
       SaveIO.load(file);
       Vars.state.set(GameState.State.playing);
       Log.info('Auto-save recovered.');
+      (playerCount === 0) && (Vars.state.serverPaused = true);
+      (playerCount === 0) && (pausedState = 'true');
     });
 }});
+
+Events.on(PlayerLeave, (event) => {
+  if (Core.settings.get('loadAutoSave', '') === 'true') {
+    if (playerCount === 0) {
+      playerCount = Groups.player.size();
+      Vars.state.serverPaused = true;
+      pausedState = 'true';
+      return;
+    };
+  };
+});
+
+Events.on(PlayerJoin, (event) => {
+  if (Core.settings.get('loadAutoSave', '') === 'true') {
+    if (pausedState === 'true') {
+      if (playerCount > 0) {
+        playerCount = Groups.player.size();
+        Vars.state.serverPaused = false;
+        pausedState = 'false';
+        return;
+      };
+    };
+  };
+});
 
 Timer.schedule(
   () => {
