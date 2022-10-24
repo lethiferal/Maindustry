2995f1fa5cd1efb2c1693f490435ef16acb7b9f2
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-10-25 00:29:50 +0530

    fix pause not working properly

--- scripts/modules/autosave.js
+++ scripts/modules/autosave.js
@@ -5,35 +5,38 @@ let pausedState;
 file = Vars.saveDirectory.child('autosave' + '.' + Vars.saveExtension);
 
 Events.on(ServerLoadEvent, (e) => {
+  playerCount = Groups.player.size();
   if (Core.settings.get('loadAutoSave', '') === 'true'){
-    playerCount = Groups.player.size();
     Core.app.post(() => {
       SaveIO.load(file);
       Vars.state.set(GameState.State.playing);
       Log.info('Auto-save recovered.');
       (playerCount === 0) && (Vars.state.serverPaused = true);
       (playerCount === 0) && (pausedState = 'true');
+      (playerCount === 0) && (Log.info('Server paused.'));
     });
 }});
 
 Events.on(PlayerLeave, (event) => {
+  playerCount = Groups.player.size();
   if (Core.settings.get('loadAutoSave', '') === 'true') {
     if (playerCount === 0) {
-      playerCount = Groups.player.size();
       Vars.state.serverPaused = true;
       pausedState = 'true';
+      Log.info('Server paused.');
       return;
     };
   };
 });
 
 Events.on(PlayerJoin, (event) => {
+  playerCount = Groups.player.size();
   if (Core.settings.get('loadAutoSave', '') === 'true') {
     if (pausedState === 'true') {
       if (playerCount > 0) {
-        playerCount = Groups.player.size();
         Vars.state.serverPaused = false;
         pausedState = 'false';
+        Log.info('Server resumed.');
         return;
       };
     };
